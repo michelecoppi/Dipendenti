caeb288c0e81594e4b89432ac1a3ee9312003c68
michelecoppi <supergimmy878@gmail.com>
2022-02-17 23:34:22 +0100

    aggiunta tasti aggiungi e rimuovi

--- pagina.js
+++ pagina.js
@@ -1,9 +1,24 @@
+function aggiungi() {
+    let table = document.getElementById("prova");
+    let tbody = table.getElementsByTagName('tbody')[0];
+    let colonne = table.getElementsByTagName('th').length;
+    let tr = document.createElement('tr');
 
+    for (let i = 0; i < colonne; i++) {
 
+        let td = document.createElement('td');
+        let tx = document.createTextNode(prompt("Inserisci testo per cella " + (i + 1), ""));
+        td.appendChild(tx);
+        tr.appendChild(td);
 
+    }
 
+    tbody.appendChild(tr);
 
+}
 
+function rimuovi(){
+    let tx = document.createTextNode(prompt("Inserisci id della riga da rimuovere"));
 
-
+}
 
